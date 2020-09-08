c61098c936cdf46e88551cbd70698164e5e708bc
jakang <jakang@wustl.edu>
2020-09-07 22:47:52 -0700

    Calculate number votes

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -3,6 +3,18 @@ import csv
 
 election_csv = os.path.join("..", "Resources", "election_data.csv")
 
-with open(election_csv) as csv_file
+with open(election_csv) as csv_file:
 	csv_reader = csv.reader(csv_file, delimiter=",")
 	header = next(csv_reader)
+	
+	# Assign variables
+	total_votes = []
+	candidates = []
+
+	for row in csv_reader:
+		# Calculate total number of votes cast
+		total_votes.append(row[0])
+		votes = len(total_votes)
+print(votes)
+		
+			
